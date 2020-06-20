fb40fdf928d75112ebf341cc2bec83a046e2497e
mehul-anshumali <mehulanshumali.ma@gmail.com>
2020-06-20 17:43:55 +0530

    Project

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pickle
+
+filename= 'spam-sms-model.pkl'
+clf= pickle.load(open(filename, 'rb'))
+cv= pickle.load(open('cv-transform.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+    	message = request.form['message']
+    	data = [message]
+    	vect = cv.transform(data).toarray()
+    	my_prediction = clf.predict(vect)
+    	return render_template('predict.html', prediction=my_prediction, text=data)
+
+if __name__ == '__main__':
+	app.run(debug=True)
